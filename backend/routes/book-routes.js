d15b10c8cef785b4a95575c5aa58b1400db03980
kennedlu <52130505+kennedlu@users.noreply.github.com>
2022-03-15 12:45:42 +0530

    Develop1242 (#9)
    * deleting node modules
    
    * add git_ignore
    
    * fix
    
    * frontend basic setup
    
    * fix: add books
    
    * fix: route changes

--- backend/routes/book-routes.js
+++ backend/routes/book-routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/books-controllers");
 
-router.get("/", booksController.getAllBooks);
-router.post("/", booksController.addBook);
+router.get("/books", booksController.getAllBooks);
+router.post("/add", booksController.addBook);
 router.get("/:id", booksController.getById);
 router.put("/:id", booksController.updateBook);
 router.delete("/:id", booksController.deleteBook);
